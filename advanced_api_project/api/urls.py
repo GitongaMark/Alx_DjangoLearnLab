4d144eb135895cfb34e2f5eb56748a00d5786e30
Mark Gitonga <markgitonga29@gmail.com>
2025-09-27 14:21:10 +0300

    Advance API Development with Django REST Framework

---
+++ advanced_api_project/api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+app_name = 'api'
+
+urlpatterns = [
+    # Book endpoints
+    path('books/', views.BookListView.as_view(), name='book-list'),
+    path('books/create/', views.BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/', views.BookUpdateView.as_view(), name='book-update-dummy'),
+    path('books/delete/', views.BookDeleteView.as_view(), name='book-delete-dummy'),
+
+    # Author endpoints
+    path('authors/', views.AuthorListView.as_view(), name='author-list'),
+    path('authors/create/', views.AuthorCreateView.as_view(), name='author-create'),
+    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('authors/<int:pk>/update/', views.AuthorUpdateView.as_view(), name='author-update'),
+    path('authors/<int:pk>/delete/', views.AuthorDeleteView.as_view(), name='author-delete'),
+]
---
+++ advanced_api_project/api/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
+from rest_framework import filters
+from .models import Book, Author
+from .serializers import BookSerializer,AuthorSerializer
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+          return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.select_related('author').all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class AuthorListView(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.AllowAny]
+
+class AuthorCreateView(generics.CreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class AuthorDetailView(generics.RetrieveAPIView):
+    queryset = Author.objects.prefetch_related('books').all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class AuthorUpdateView(generics.UpdateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class AuthorDeleteView(generics.DestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.IsAuthenticated]
